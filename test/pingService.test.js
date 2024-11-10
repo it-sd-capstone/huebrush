dd3be7bee5d7e94f4f713b9479157369d075f496
Skyler Godfrey <sgodfrey@cvtc.edu>
2024-11-10 15:27:10 -0600

    Update unit test to PingService file

---
+++ test/pingService.test.js
@@ -0,0 +1,16 @@
+import { describe, it, expect } from 'vitest';
+import { pingDomain } from './pingService';
+
+const url = "huebrush.com";
+
+describe('pingDomain', () => {
+  it('should successfully ping ' + url, async () => {
+    const status = await pingDomain(url);
+    expect(status).toBe(200);
+  });
+
+  it('should successfully ping www.' + url, async () => {
+    const status = await pingDomain(url);
+    expect(status).toBe(200);
+  });
+});
