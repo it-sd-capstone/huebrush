73fcac727d4dee1f9089c441191b00a0697958ea
Skyler Godfrey <sgodfrey@cvtc.edu>
2024-11-11 18:42:35 -0600

    Revert controller functionality

--- controller.js
+++ controller.js
@@ -1,4 +1,4 @@
-let box = document.getElementById("#myBox");
+let box = document.querySelector('#myBox');
 let moveBy = 10;
 let opacity = 1;
 
@@ -8,60 +8,55 @@ window.addEventListener('load', () => {
   box.style.top = 0;
 });
 
+function drainColor() {
+  let boxColorGrabber = window.getComputedStyle(box);
+  let boxColor = boxColorGrabber.backgroundColor;
 
-// function drainColor() {
-//   let boxColorGrabber = window.getComputedStyle(box);
-//   let boxColor = boxColorGrabber.backgroundColor;
+  if (opacity > 0) {
+    opacity -= 0.0;
 
-//   if (opacity > 0) {
-//     opacity -= 0.0;
+    let rgbValues = boxColor.match(/\d+/g); 
+    if (rgbValues) {
+      box.style.backgroundColor = `rgba(${rgbValues[0]}, ${rgbValues[1]}, ${rgbValues[2]}, ${opacity})`;
+    }
 
-//     let rgbValues = boxColor.match(/\d+/g); 
-//     if (rgbValues) {
-//       box.style.backgroundColor = `rgba(${rgbValues[0]}, ${rgbValues[1]}, ${rgbValues[2]}, ${opacity})`;
-//     }
-
-//   } else if (opacity <= 0) {
-//     console.log("Game Over");
-//   }
-// }
+  } else if (opacity <= 0) {
+    console.log("Game Over");
+  }
+}
 
 document.addEventListener('keydown', (e) => {
   switch (e.key) {
     case 'ArrowUp':
     case 'w':
-      console.log('Up arrow pressed');
       box.style.top = parseInt(box.style.top) - moveBy + 'px';
-      checkKey();
-      checkGate();
+      drainColor();
+      checkKey()
       break;
     case 'ArrowDown':
     case 's':
-      console.log('Down arrow pressed');
       box.style.top = parseInt(box.style.top) + moveBy + 'px';
-      checkKey();
-      checkGate();
+      drainColor();
+      checkKey()
       break;
     case 'ArrowLeft':
     case 'a':
-      console.log('Left arrow pressed');
       box.style.left = parseInt(box.style.left) - moveBy + 'px';
-      checkKey();
-      checkGate();
+      drainColor();
+      checkKey()
       break;
     case 'ArrowRight':
     case 'd':
-      console.log('Right arrow pressed');
       box.style.left = parseInt(box.style.left) + moveBy + 'px';
-      checkKey();
-      checkGate();
+      drainColor();
+      checkKey()
       break;
   }
 });
 
 function checkKey() {
-    if (box.style.top == key.style.top && box.style.left == key.style.left) {
-        box.style.background = "green";
-        key.remove();
-    }
+  if (parseInt(box.style.left) == parseInt(key.style.left) && parseInt(box.style.top) == parseInt(key.style.top)) {
+    box.style.background = "purple";
+    key.remove();
+  }
 }
